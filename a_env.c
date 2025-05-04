073a55341678e9b702c38a0d773b52b68ef04434
Raf Petrosyan <raf.petrosian@gmail.com>
2025-05-04 10:29:19 +0400

    s

--- a_env.c
+++ a_env.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "minishell.h"
 
 int	get_key_len(char *str)
 {
--- a_export_sort.c
+++ a_export_sort.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "minishell.h"
 
 int	ft_strcmp(char *str1, char *str2)
 {
